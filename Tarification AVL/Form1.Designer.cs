ead239d58440433f456a9a6e0561f2c02c1ef67a
Alexis <72019466+alexisepi@users.noreply.github.com>
2023-01-09 10:12:26 +0100

    avl v1.0
    adding base components

---
+++ Tarification AVL/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Tarification_AVL
+{
+    partial class HOME
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textBoxDpt = new System.Windows.Forms.TextBox();
+            this.textBoxPoids = new System.Windows.Forms.TextBox();
+            this.labelDpt = new System.Windows.Forms.Label();
+            this.labelPoids = new System.Windows.Forms.Label();
+            this.labelChoix = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSearch.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSearch.Location = new System.Drawing.Point(3, 345);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(190, 67);
+            this.buttonSearch.TabIndex = 0;
+            this.buttonSearch.Text = "Rechercher";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // textBoxDpt
+            // 
+            this.textBoxDpt.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBoxDpt.Location = new System.Drawing.Point(213, 75);
+            this.textBoxDpt.Name = "textBoxDpt";
+            this.textBoxDpt.Size = new System.Drawing.Size(204, 20);
+            this.textBoxDpt.TabIndex = 1;
+            // 
+            // textBoxPoids
+            // 
+            this.textBoxPoids.Location = new System.Drawing.Point(213, 174);
+            this.textBoxPoids.Name = "textBoxPoids";
+            this.textBoxPoids.Size = new System.Drawing.Size(204, 20);
+            this.textBoxPoids.TabIndex = 2;
+            // 
+            // labelDpt
+            // 
+            this.labelDpt.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelDpt.AutoSize = true;
+            this.labelDpt.Font = new System.Drawing.Font("Franklin Gothic Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDpt.Location = new System.Drawing.Point(3, 75);
+            this.labelDpt.Name = "labelDpt";
+            this.labelDpt.Size = new System.Drawing.Size(106, 21);
+            this.labelDpt.TabIndex = 3;
+            this.labelDpt.Text = "Département :";
+            // 
+            // labelPoids
+            // 
+            this.labelPoids.AutoSize = true;
+            this.labelPoids.Font = new System.Drawing.Font("Franklin Gothic Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPoids.Location = new System.Drawing.Point(3, 171);
+            this.labelPoids.Name = "labelPoids";
+            this.labelPoids.Size = new System.Drawing.Size(55, 21);
+            this.labelPoids.TabIndex = 4;
+            this.labelPoids.Text = "Poids :";
+            // 
+            // labelChoix
+            // 
+            this.labelChoix.AutoSize = true;
+            this.labelChoix.Font = new System.Drawing.Font("Franklin Gothic Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelChoix.Location = new System.Drawing.Point(3, 0);
+            this.labelChoix.Name = "labelChoix";
+            this.labelChoix.Size = new System.Drawing.Size(197, 21);
+            this.labelChoix.TabIndex = 5;
+            this.labelChoix.Text = "Aucune information saisie...";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 63F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37F));
+            this.tableLayoutPanel1.Controls.Add(this.labelChoix, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(435, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(374, 96);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.labelDpt, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.textBoxDpt, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonSearch, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.textBoxPoids, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.labelPoids, 0, 1);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel2.MaximumSize = new System.Drawing.Size(420, 430);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 73F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(420, 415);
+            this.tableLayoutPanel2.TabIndex = 7;
+            // 
+            // HOME
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 463);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "HOME";
+            this.Text = "Tarification des transporteurs";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.TextBox textBoxDpt;
+        private System.Windows.Forms.TextBox textBoxPoids;
+        private System.Windows.Forms.Label labelDpt;
+        private System.Windows.Forms.Label labelPoids;
+        private System.Windows.Forms.Label labelChoix;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+    }
+}
+
---
+++ Tarification AVL/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Configuration;
+
+namespace Tarification_AVL
+{
+    public partial class HOME : Form
+    {
+        public HOME()
+        {
+            InitializeComponent();
+
+            this.MinimumSize = new System.Drawing.Size(690, 250); // Taille minimale de la fenêtre en pixels
+            this.MaximumSize = new System.Drawing.Size(1100, 670); // Taille minimale de la fenêtre en pixels
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            int columnNumber = 0;
+
+            // Récupération de l'instance de l'application Excel
+            Excel.Application excelApp = new Excel.Application();
+
+            // Ouverture du fichier Excel
+            Excel.Workbook workbook = excelApp.Workbooks.Open("C:\\Users\\Mylan\\Documents\\Tarification AVL\\Tarifs.xlsx");
+
+            // Sélection de la première feuille de calcul
+            Excel.Worksheet worksheet = workbook.Worksheets[1];
+
+            //excelApp.Visible = true;
+
+            // Liste des départements de France dans l'ordre
+            string[] departements = {
+                "Ain", "Aisne", "Allier", "Alpes de Haute-Provence", "Hautes-Alpes", "Alpes-Maritimes", "Ardèche", "Ardennes",
+                "Ariège", "Aube", "Aude", "Aveyron", "Bouches-du-Rhône", "Calvados", "Cantal", "Charente", "Charente-Maritime",
+                "Cher", "Corrèze", "Corse-du-Sud", "Côte-d'Or", "Côtes d'Armor", "Creuse", "Dordogne", "Doubs",
+                "Drôme", "Eure", "Eure-et-Loir", "Finistère", "Gard", "Haute-Garonne", "Gers", "Gironde", "Hérault", "Ille-et-Vilaine",
+                "Indre", "Indre-et-Loire", "Isère", "Jura", "Landes", "Loir-et-Cher", "Loire", "Haute-Loire", "Loire-Atlantique", "Loiret",
+                "Lot", "Lot-et-Garonne", "Lozère", "Maine-et-Loire", "Manche", "Marne", "Haute-Marne", "Mayenne", "Meurthe-et-Moselle", "Meuse",
+                "Morbihan", "Moselle", "Nièvre", "Nord", "Oise", "Orne", "Pas-de-Calais", "Puy-de-Dôme", "Pyrénées-Atlantiques", "Hautes-Pyrénées",
+                "Pyrénées-Orientales", "Bas-Rhin", "Haut-Rhin", "Rhône", "Haute-Saône", "Saône-et-Loire", "Sarthe", "Savoie", "Haute-Savoie", "Paris",
+                "Seine-Maritime", "Seine-et-Marne", "Yvelines", "Deux-Sèvres", "Somme", "Tarn", "Tarn-et-Garonne", "Var", "Vaucluse", "Vendée", "Vienne",
+                "Haute-Vienne", "Vosges", "Yonne", "Territoire de Belfort", "Essonne", "Hauts-de-Seine", "Seine-Saint-Denis", "Val-de-Marne", "Val-d'Oise"
+            };
+
+            // Déclarez une variable booléenne pour enregistrer si la valeur est un entier ou non
+            bool isInteger = false;
+
+            // Récupérez la valeur du textbox
+            string value = textBoxDpt.Text;
+
+            // Utilisez la méthode TryParse de la classe Int32 pour essayer de convertir la valeur en entier
+            isInteger = Int32.TryParse(value, out int result);
+
+            // Si la conversion a réussi, la variable isInteger sera égale à true, sinon elle sera égale à false
+            if (isInteger)
+            {
+                int index = int.Parse(textBoxDpt.Text) - 1;
+                if (index >= 0 && index < departements.Length)
+                {
+                    // Récupération de la valeur saisie dans le textBox
+                    string textBoxValue = textBoxDpt.Text;
+
+                    // Conversion de la valeur saisie en entier
+                    int rowNumber = int.Parse(textBoxValue);
+
+                    if (textBoxPoids.Text == "")
+                    {
+                        labelChoix.Text = $"Veuillez saisir le poids";
+                    }
+                    else if(textBoxPoids.Text != "")
+                    {
+                        float poids = Single.Parse(textBoxPoids.Text);
+
+                        if (poids <= 9)
+                        {
+                            columnNumber = 2;
+                        }
+                        else if (poids <= 19)
+                        {
+                            columnNumber = 5;
+                        }
+                        else if (poids <= 29)
+                        {
+                            columnNumber = 8;
+                        }
+                        else if (poids <= 39)
+                        {
+                            columnNumber = 11;
+                        }
+                        else if (poids <= 49)
+                        {
+                            columnNumber = 14;
+                        }
+                        else if (poids <= 59)
+                        {
+                            columnNumber = 17;
+                        }
+                        else if (poids <= 69)
+                        {
+                            columnNumber = 20;
+                        }
+                        else if (poids <= 79)
+                        {
+                            columnNumber = 23;
+                        }
+                        else if (poids <= 100)
+                        {
+                            columnNumber = 26;
+                        }
+                        else if (poids <= 499)
+                        {
+                            columnNumber = 29;
+                        }
+                        else if (poids <= 750)
+                        {
+                            columnNumber = 32;
+                        }
+
+                        // Lecture de la valeur de la cellule Bx (x étant la ligne correspondant à la valeur saisie)
+                        object value1 = worksheet.Cells[rowNumber + 2, columnNumber].Value;
+                        object value2 = worksheet.Cells[rowNumber + 2, columnNumber + 1].Value;
+                        object value3 = worksheet.Cells[rowNumber + 2, columnNumber + 2].Value;
+
+                        int VAL1 = Convert.ToInt32(value1);
+                        int VAL2 = Convert.ToInt32(value2);
+                        int VAL3 = Convert.ToInt32(value3);
+
+                        int min = Math.Min(Math.Min(VAL1, VAL2), VAL3);
+
+                        // Si T1 est la plus petite
+                        if (min == VAL1)
+                        {
+                            labelChoix.Text = $"Le meilleur transporteur pour le département de {departements[index]} sera le transporteur 1 : T1 : " + value1;
+                        }
+                        // Si T2 est la plus petite
+                        else if (min == VAL2)
+                        {
+                            labelChoix.Text = $"Le meilleur transporteur pour le département de {departements[index]} sera le transporteur 2 : T2 : " + value2;
+                        }
+                        // Si T3 est la plus petite
+                        else
+                        {
+                            labelChoix.Text = $"Le meilleur transporteur pour le département de {departements[index]} sera le transporteur 3 : T3 : " + value3;
+                        }
+                    }
+
+                    // Fermeture du fichier Excel
+                    workbook.Close();
+                }
+                else if (index >= 96)
+                {
+                    labelChoix.Text = $"Information saisie erronée";
+                }
+            }
+            else
+            {
+                labelChoix.Text = $"Aucune information saisie...";
+            }
+        }
+    }
+}
